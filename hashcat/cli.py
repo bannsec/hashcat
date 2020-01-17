99dc79854ba9ea45cdfd017245a1849b03a4f29e
bannsec <whootandahalf@gmail.com>
2020-01-17 18:46:26 -0500

    Fixing hashcat's mess...

--- hashcat/cli.py
+++ hashcat/cli.py
@@ -6,9 +6,16 @@ import sys
 
 def main():
     args = sys.argv[1:] if len(sys.argv) > 1 else []
+
+    # Because hashcat devs can't figure out how to handle paths...
+    # https://github.com/hashcat/hashcat/issues/2288
+    for i, arg in enumerate(args):
+        # Implicitly determine if this is a path and if so, make it absolute
+        if os.path.exists(arg):
+            args[i] = os.path.abspath(arg)
     
     # Light passthrough
-    subprocess.run([hashcat_exe_path] + args)
+    subprocess.run([hashcat_exe_path] + args, cwd=hashcat_dir)
 
 bits = platform.architecture()[0][:-3]
 ext = ".exe" if platform.uname().system == "Windows" else ".bin"
